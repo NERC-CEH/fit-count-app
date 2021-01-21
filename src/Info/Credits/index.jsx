4198effe0fdda9b1b493a21d73269fe9c9d53738
Vilius Stankaitis <vilius@stankaitis.com>
2021-01-21 14:22:52 +0000

    Update credits list

--- src/Info/Credits/index.jsx
+++ src/Info/Credits/index.jsx
@@ -14,25 +14,11 @@ const Credits = () => (
         </P>
         <P skipTranslation>
           <ul>
+            <li>Claire Carvell (UKCEH)</li>
+            <li>Martin Harvey (UKCEH)</li>
             <li>Karolis Kazlauskis (Flumens)</li>
             <li>Vilius Stankaitis (Flumens)</li>
-            <li>Lorrum ipsum</li>
-            <li>Lorrum ipsum</li>
-            <li>Lorrum ipsum</li>
-            <li>Lorrum ipsum</li>
-            <li>Lorrum ipsum</li>
-            <li>Lorrum ipsum</li>
           </ul>
-        </P>
-        <P>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-          vulputate a neque sit amet cursus. Sed lorem libero, feugiat quis nibh
-          sit amet, tempus fringilla tortor. Pellentesque habitant morbi
-          tristique senectus et netus et malesuada fames ac turpis egestas.
-          Donec feugiat vehicula facilisis. Nullam gravida efficitur venenatis.
-          Vestibulum et laoreet nibh, in imperdiet justo. Vivamus fringilla
-          mauris metus, ut euismod libero dapibus et. Integer nec turpis
-          volutpat, auctor felis vulputate, gravida erat.
         </P>
       </Section>
     </Main>
