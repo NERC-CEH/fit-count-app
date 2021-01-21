95a978a1ea4f29b7092187b1cd145d4f42393135
Vilius Stankaitis <vilius@stankaitis.com>
2021-01-21 09:24:20 +0000

    Clean Up!

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { observer } from 'mobx-react';
-import Menu from 'Components/Menu';
 import Home from './Home';
-import Info from './Info/router';
 
 const HomeRedirect = () => {
   return <Redirect to="home" />;
@@ -14,11 +12,9 @@ const HomeRedirect = () => {
 const App = () => (
   <IonApp>
     <IonReactRouter>
-      <Menu />
       <IonRouterOutlet id="main">
         <Route exact path="/" component={HomeRedirect} />
         <Route path="/home" component={Home} />
-        {Info}
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
