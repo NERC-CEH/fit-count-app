57805f4cb259ff8325d1a03a3064507204386a5a
Vilius Stankaitis <vilius@stankaitis.com>
2021-01-29 14:42:22 +0000

    Update credits

--- src/Info/Credits/index.jsx
+++ src/Info/Credits/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Header, Page, Main, Section } from '@apps';
 import './styles.scss';
 
-const { P } = Section;
+const { H, P } = Section;
 
 const Credits = () => (
   <Page id="credits">
@@ -19,6 +19,37 @@ const Credits = () => (
             <li>Karolis Kazlauskis (Flumens)</li>
             <li>Vilius Stankaitis (Flumens)</li>
           </ul>
+        </P>
+        <H>Graphics</H>
+        <P>
+          Icons made by{' '}
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik
+          </a>{' '}
+          from
+          <br />
+          <br />
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </P>
+        <P>
+          App guide illustrations made by
+          <br />
+          <br />
+          <a href="https://www.divulgare.net/" title="Divulgare">
+            www.divulgare.net
+          </a>
+          <br />
+          <br />
+          <a href="https://flic.kr/p/2k2HaKk" title="Flic">
+            https://flic.kr/p/2k2HaKk
+          </a>
+          <br />
+          <br />
+          <a href="hhttp://www.clker.com/" title="Clker">
+            http://www.clker.com/
+          </a>
         </P>
       </Section>
     </Main>
