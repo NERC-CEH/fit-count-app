9ddb506e029f48508bc12911365d0f5a2d42b9cf
Vilius Stankaitis <vilius@stankaitis.com>
2021-02-19 14:36:40 +0000

    #26 Fix survey next button alignment

--- src/Survey/Components/Footer/index.jsx
+++ src/Survey/Components/Footer/index.jsx
@@ -9,17 +9,16 @@ import './styles.scss';
 function Footer(props) {
   const { isEnabled, link } = props;
 
-  const buttonStyles = isEnabled ? 'next-button' : 'next-button disable';
-
   const navigateTo = isEnabled ? link : false;
 
   return (
     <div id="survey-footer">
       <IonItem
         lines="none"
-        className={buttonStyles}
-        detail={isEnabled}
+        className="next-button"
         routerLink={navigateTo}
+        detail
+        disabled={!isEnabled}
       >
         <IonLabel className="ion-text-center">
           <T>Next</T>
