2c05ee5bf8c82e011c6368532d2d5e36d3492620
Vilius Stankaitis <vilius@stankaitis.com>
2021-03-09 16:21:08 +0000

    Fix Footer flickering

--- src/Survey/Components/Footer/index.jsx
+++ src/Survey/Components/Footer/index.jsx
@@ -3,26 +3,33 @@ import PropTypes from 'prop-types';
 import exact from 'prop-types-exact';
 import { observer } from 'mobx-react';
 import { Trans as T } from 'react-i18next';
-import { IonItem, IonLabel, IonFooter } from '@ionic/react';
+import { IonItem, IonLabel, IonFooter, IonIcon } from '@ionic/react';
+import clsx from 'clsx';
 import './styles.scss';
+import { flagOutline, chevronForwardOutline } from 'ionicons/icons';
 
 function Footer(props) {
-  const { isEnabled, link } = props;
+  const { isEnabled, link, title } = props;
 
   const navigateTo = isEnabled ? link : false;
+
+  const footerTitle = title || 'Next';
+
+  const icon = title ? flagOutline : chevronForwardOutline;
 
   return (
     <IonFooter className="ion-no-border">
       <div id="survey-footer">
         <IonItem
           lines="none"
-          className="next-button"
+          className={clsx('next-button', title && 'finish-button')}
           routerLink={navigateTo}
-          detail
           disabled={!isEnabled}
+          mode="md"
         >
+          <IonIcon slot="end" color="light" icon={icon} />
           <IonLabel className="ion-text-center">
-            <T>Next</T>
+            <T>{footerTitle}</T>
           </IonLabel>
         </IonItem>
       </div>
@@ -33,6 +40,7 @@ function Footer(props) {
 Footer.propTypes = exact({
   isEnabled: PropTypes.bool.isRequired,
   link: PropTypes.string.isRequired,
+  title: PropTypes.string,
 });
 
 export default observer(Footer);
