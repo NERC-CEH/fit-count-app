ca8a7e1949a4b77e61259d5126e99546b5201ca2
Karolis Kazlauskis <karolis@flumens.io>
2021-03-12 17:47:52 +0000

    #39 Enable upload

--- src/Survey/WeatherWind/index.jsx
+++ src/Survey/WeatherWind/index.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 import exact from 'prop-types-exact';
 import { Page, Attr, Main } from '@apps';
 import appModel from 'models/app';
-import { NavContext } from '@ionic/react';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import { NavContext, IonButton } from '@ionic/react';
+import CustomAlert from '../Components/CustomAlert';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+import './styles.scss';
 
 const PAGE_INDEX = 10;
-
-const NEXT_PAGE = '/home/surveys';
 
 class WeatherWind extends React.Component {
   static contextType = NavContext;
@@ -22,6 +22,8 @@ class WeatherWind extends React.Component {
     location: PropTypes.object, // eslint-disable-line
   });
 
+  state = { showThanks: false };
+
   _processDraft = async () => {
     const { sample } = this.props;
 
@@ -32,25 +34,34 @@ class WeatherWind extends React.Component {
     sample.save();
   };
 
+  goHome = () => {
+    this.context.navigate('/home/surveys', 'root');
+  };
+
+  uploadSurvey = () => {
+    const { sample } = this.props;
+    sample.saveRemote();
+
+    this.goHome();
+  };
+
   onFinish = async () => {
     const { sample } = this.props;
 
     if (!sample.metadata.saved) {
       await this._processDraft();
     }
+
+    this.setState({ showThanks: true });
   };
 
   onValueChange = value => {
     const { sample } = this.props;
     sample.attrs['weather-wind'] = value;
     sample.save();
-
-    this.onFinish();
   };
 
-  isValueValid = () =>
-    !!this.props.sample.attrs['weather-wind'] &&
-    !!this.props.sample.metadata.saved;
+  isValueValid = () => !!this.props.sample.attrs['weather-wind'];
 
   render() {
     const { sample } = this.props;
@@ -74,8 +85,20 @@ class WeatherWind extends React.Component {
         <Footer
           isEnabled={this.isValueValid()}
           title="Save my count"
-          link={NEXT_PAGE}
+          onClick={this.onFinish}
         />
+
+        {this.state.showThanks && (
+          <CustomAlert>
+            <h1>Thanks!</h1>
+            <IonButton color="secondary" onClick={this.uploadSurvey}>
+              Upload
+            </IonButton>
+            <IonButton color="medium" onClick={this.goHome}>
+              Go Home
+            </IonButton>
+          </CustomAlert>
+        )}
       </Page>
     );
   }
