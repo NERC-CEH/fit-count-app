c4f1096b6496a0193e986afb60fbf87239e23265
Vilius Stankaitis <vilius@stankaitis.com>
2021-03-22 10:23:15 +0000

    #89 Fix language change translation

--- src/Home/Menu/Main/index.jsx
+++ src/Home/Menu/Main/index.jsx
@@ -28,148 +28,141 @@ import languages from 'common/languages';
 import flumensLogo from 'common/images/flumens.svg';
 import getURLSpecificToLanguage from 'common/Components/getURLSpecificToLanguage';
 
-@observer
-class MenuComponent extends React.Component {
-  static propTypes = exact({
-    onToggle: PropTypes.func.isRequired,
-    sendAnalytics: PropTypes.bool.isRequired,
-    config: PropTypes.object.isRequired,
-    language: PropTypes.string.isRequired,
-    // country: PropTypes.string.isRequired,
-    isLoggedIn: PropTypes.bool.isRequired,
-    user: PropTypes.object.isRequired,
-    logOut: PropTypes.func.isRequired,
-  });
+function MenuComponent({
+  onToggle,
+  sendAnalytics,
+  config,
+  language,
+  // country,
+  isLoggedIn,
+  user,
+  logOut,
+}) {
+  const onSendAnalyticsToggle = checked => onToggle('sendAnalytics', checked);
 
-  render() {
-    const {
-      onToggle,
-      sendAnalytics,
-      config,
-      language,
-      // country,
-      isLoggedIn,
-      user,
-      logOut,
-    } = this.props;
+  // const countryName = ({ value }) => value === country;
+  // const selectedCountries = countries.find(countryName) || {};
 
-    const onSendAnalyticsToggle = checked => onToggle('sendAnalytics', checked);
+  return (
+    <Main>
+      <h1>
+        <T>Menu</T>
+      </h1>
 
-    // const countryName = ({ value }) => value === country;
-    // const selectedCountries = countries.find(countryName) || {};
-
-    return (
-      <Main>
-        <h1>
-          <T>Menu</T>
-        </h1>
-
-        <IonList lines="full">
-          <IonItemDivider>
-            <T>User</T>
-          </IonItemDivider>
-          <div className="rounded">
-            {isLoggedIn && (
-              <IonItem detail id="logout-button" onClick={logOut}>
-                <IonIcon icon={exitOutline} size="small" slot="start" />
-                <T>Logout</T>
-                {': '}
-                {user.fullName}
-              </IonItem>
-            )}
-
-            {!isLoggedIn && (
-              <IonItem routerLink="/user/login" detail>
-                <IonIcon icon={personOutline} size="small" slot="start" />
-                <T>Login</T>
-              </IonItem>
-            )}
-
-            {!isLoggedIn && (
-              <IonItem routerLink="/user/register" detail>
-                <IonIcon icon={personAddOutline} size="small" slot="start" />
-                <T>Register</T>
-              </IonItem>
-            )}
-          </div>
-
-          <IonItemDivider>
-            <T>Info</T>
-          </IonItemDivider>
-          <div className="rounded">
-            <IonItem routerLink="/info/about" detail>
-              <IonIcon
-                icon={informationCircleOutline}
-                size="small"
-                slot="start"
-              />
-              <T>About</T>
+      <IonList lines="full">
+        <IonItemDivider>
+          <T>User</T>
+        </IonItemDivider>
+        <div className="rounded">
+          {isLoggedIn && (
+            <IonItem detail id="logout-button" onClick={logOut}>
+              <IonIcon icon={exitOutline} size="small" slot="start" />
+              <T>Logout</T>
+              {': '}
+              {user.fullName}
             </IonItem>
-            <IonItem
-              href={getURLSpecificToLanguage('privacy-notice')}
-              detail
-              detailIcon={openOutline}
-            >
-              <IonIcon icon={lockClosedOutline} size="small" slot="start" />
-              <T>Privacy Policy</T>
-            </IonItem>
-            <IonItem routerLink="/info/credits" detail>
-              <IonIcon icon={heartOutline} size="small" slot="start" />
-              <T>Credits</T>
-            </IonItem>
-          </div>
+          )}
 
-          <IonItemDivider>
-            <T>Settings</T>
-          </IonItemDivider>
+          {!isLoggedIn && (
+            <IonItem routerLink="/user/login" detail>
+              <IonIcon icon={personOutline} size="small" slot="start" />
+              <T>Login</T>
+            </IonItem>
+          )}
 
-          <div className="rounded">
-            <MenuAttrItem
-              routerLink="/settings/language"
-              value={languages[language]}
-              label="Language"
-              icon={languageOutline}
-              skipValueTranslation
+          {!isLoggedIn && (
+            <IonItem routerLink="/user/register" detail>
+              <IonIcon icon={personAddOutline} size="small" slot="start" />
+              <T>Register</T>
+            </IonItem>
+          )}
+        </div>
+
+        <IonItemDivider>
+          <T>Info</T>
+        </IonItemDivider>
+        <div className="rounded">
+          <IonItem routerLink="/info/about" detail>
+            <IonIcon
+              icon={informationCircleOutline}
+              size="small"
+              slot="start"
             />
+            <T>About</T>
+          </IonItem>
+          <IonItem
+            href={getURLSpecificToLanguage('privacy-notice')}
+            detail
+            detailIcon={openOutline}
+          >
+            <IonIcon icon={lockClosedOutline} size="small" slot="start" />
+            <T>Privacy Policy</T>
+          </IonItem>
+          <IonItem routerLink="/info/credits" detail>
+            <IonIcon icon={heartOutline} size="small" slot="start" />
+            <T>Credits</T>
+          </IonItem>
+        </div>
 
-            {/* <MenuAttrItem
+        <IonItemDivider>
+          <T>Settings</T>
+        </IonItemDivider>
+
+        <div className="rounded">
+          <MenuAttrItem
+            routerLink="/settings/language"
+            value={languages[language]}
+            label="Language"
+            icon={languageOutline}
+            routerOptions={{ unmount: true }} // Pick a new language on return
+            skipValueTranslation
+          />
+
+          {/* <MenuAttrItem
               routerLink="/settings/country"
               value={selectedCountries.label}
               label="Country"
               icon={globeOutline}
             /> */}
 
-            <IonItem className="exception-rounded">
-              <IonIcon icon={shareSocialOutline} size="small" slot="start" />
-              <IonLabel>
-                <T>Share App Analytics</T>
-              </IonLabel>
-              <Toggle
-                onToggle={onSendAnalyticsToggle}
-                checked={sendAnalytics}
-              />
-            </IonItem>
+          <IonItem className="exception-rounded">
+            <IonIcon icon={shareSocialOutline} size="small" slot="start" />
+            <IonLabel>
+              <T>Share App Analytics</T>
+            </IonLabel>
+            <Toggle onToggle={onSendAnalyticsToggle} checked={sendAnalytics} />
+          </IonItem>
 
-            <InfoMessage color="medium">
-              Share app crash data so we can make the app more reliable.
-            </InfoMessage>
+          <InfoMessage color="medium">
+            Share app crash data so we can make the app more reliable.
+          </InfoMessage>
+        </div>
+
+        <div className="flumens-section">
+          <div>
+            <a href="https://flumens.io">
+              <img src={flumensLogo} alt="flumens-logo" />
+            </a>
+
+            <p className="app-version">
+              <T>App version</T>: {`v${config.version} (${config.build})`}
+            </p>
           </div>
-
-          <div className="flumens-section">
-            <div>
-              <a href="https://flumens.io">
-                <img src={flumensLogo} alt="flumens-logo" />
-              </a>
-
-              <p className="app-version">
-                <T>App version</T>: {`v${config.version} (${config.build})`}
-              </p>
-            </div>
-          </div>
-        </IonList>
-      </Main>
-    );
-  }
+        </div>
+      </IonList>
+    </Main>
+  );
 }
 
-export default MenuComponent;
+MenuComponent.propTypes = exact({
+  onToggle: PropTypes.func.isRequired,
+  sendAnalytics: PropTypes.bool.isRequired,
+  config: PropTypes.object.isRequired,
+  language: PropTypes.string.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+  user: PropTypes.object.isRequired,
+  logOut: PropTypes.func.isRequired,
+  // country: PropTypes.string.isRequired,
+});
+
+export default observer(MenuComponent);
