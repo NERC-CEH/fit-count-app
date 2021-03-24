d33bdc1b0a006c3bac9e2f33eb0275c0c1490453
Vilius Stankaitis <vilius@stankaitis.com>
2021-03-24 08:58:43 +0000

    #93 Update Credits page

--- src/Info/Credits/index.jsx
+++ src/Info/Credits/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { IonGrid, IonList, IonRow, IonCol } from '@ionic/react';
 import { Header, Page, Main, Section } from '@apps';
 import { Trans as T } from 'react-i18next';
+import surpassLogo from './images/SURPASS_logo.png';
+import pomsLogo from './images/PoMS_Logo_WithStrap_Positive_Black.png';
+import pMSkyLogo from './images/p-MS-ky.png';
 import './styles.scss';
 
 const { H, P } = Section;
@@ -10,104 +14,124 @@ const Credits = () => (
     <Header title="Credits" />
     <Main>
       <Section>
-        <P>We are grateful to all the people who helped to create this app:</P>
-
         <P skipTranslation>
-          <ul className="disable-bullet-points">
-            <li>
-              <b>Claire Carvell</b> (UKCEH)
-            </li>
-            <li>
-              <b>Martin Harvey</b> (UKCEH)
-            </li>
-            <li>
-              <b>David Roy</b> (UKCEH)
-            </li>
-            <li>
-              <b>Nadine Mitschunas</b> (UKCEH)
-            </li>
-            <li>
-              <b>Karolis Kazlauskis</b> (Flumens)
-            </li>
-            <li>
-              <b>Vilius Stankaitis</b> (Flumens)
-            </li>
-          </ul>
+          <T>This app has been created by the</T>{' '}
+          <a href="https://www.ceh.ac.uk/">UK Centre for Ecology & Hydrology</a>{' '}
+          <T>with technical development by</T>{' '}
+          <a href="https://flumens.io/">Flumens</a>.
         </P>
 
         <P skipTranslation>
-          <T>Team members of the following projects</T>:
-          <ul>
-            <li>UK Pollinator Monitoring Scheme (UK PoMS)</li>
-            <li>
-              Safeguarding pollinators and pollination services in a changing
-              world (SURPASS2)
-            </li>
-            <li>Pollinator Monitoring Scheme – Kýpros (PoMS-Ký)</li>
-          </ul>
+          <T>
+            App development has been supported by the funders and partners of
+            these pollinator projects around the world
+          </T>
+          :
         </P>
 
-        <div className="list-background">
-          <H>Graphics:</H>
-          <P skipTranslation>
-            <T>FIT Count logo</T>: Heather Harris (UKCEH)
-          </P>
+        <ul>
+          <li>
+            UK Pollinator Monitoring Scheme (UK PoMS){' '}
+            <a href="http://ukpoms.org.uk">http://ukpoms.org.uk</a>
+          </li>
 
-          <P skipTranslation>
-            <T>Photos of flowers and insects by:</T>
-            <ul>
-              <li>Martin Harvey</li>
-              <li>Nadine Mitschunas</li>
-            </ul>
-          </P>
-
-          <P skipTranslation>
-            <T>Thanks to the following for additional photos</T>:
-            <ul>
-              <li>Aiwok via Wikimedia (solitary bee)</li>
-              <li>Gail Hampshire via Wikimedia (solitary wasp)</li>
-              <li>Gilles San Martin vias Wikispecies (Buddleja)</li>
-              <li>Malcolm Storey via BioImages (honeybee)</li>
-              <li>Peter O'Connor via Flickr CC (Hogweed)</li>
-              <li>Trounce via Wikimedia (wasp)</li>
-            </ul>
-          </P>
-
-          <P>
-            Icons made by{' '}
-            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
-              Freepik
-            </a>{' '}
-            from
-            <br />
-            <br />
-            <a href="https://www.flaticon.com/" title="Flaticon">
-              www.flaticon.com
+          <li>
+            Pollinator Monitoring Scheme Kýpros: PoMS-Ký{' '}
+            <a href="https://www.ris-ky.info/poms-ky">
+              https://www.ris-ky.info/poms-ky
             </a>
-          </P>
-          <P>
-            App guide illustrations made by:
-            <ul>
-              <li>
-                <a href="https://www.divulgare.net/" title="Divulgare">
-                  www.divulgare.net
-                </a>
-              </li>
+          </li>
 
-              <li>
-                <a href="https://flic.kr/p/2k2HaKk" title="Flic">
-                  https://flic.kr/p/2k2HaKk
-                </a>
-              </li>
+          <li>
+            Safeguarding pollinators and pollination services in a changing
+            world (SURPASS2){' '}
+            <a href="https://bee-surpass.org">https://bee-surpass.org</a>
+          </li>
+        </ul>
 
-              <li>
-                <a href="hhttp://www.clker.com/" title="Clker">
-                  http://www.clker.com/
+        <IonList className="list-background">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <a href="http://ukpoms.org.uk">
+                  <img src={pomsLogo} />
                 </a>
-              </li>
-            </ul>
-          </P>
-        </div>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <a href="https://www.ris-ky.info/poms-ky">
+                  <img src={pMSkyLogo} />
+                </a>
+              </IonCol>
+              <IonCol>
+                <a href="https://bee-surpass.org">
+                  <img src={surpassLogo} />
+                </a>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonList>
+
+        <H>Graphics:</H>
+        <P skipTranslation>
+          <T>FIT Count logo</T>: Heather Harris (UKCEH)
+        </P>
+
+        <P skipTranslation>
+          <T>Photos of flowers and insects by:</T>
+          <ul>
+            <li>Martin Harvey</li>
+            <li>Nadine Mitschunas</li>
+          </ul>
+        </P>
+
+        <P skipTranslation>
+          <T>Thanks to the following for additional photos</T>:
+          <ul>
+            <li>Aiwok via Wikimedia</li>
+            <li>Gail Hampshire via Wikimedia</li>
+            <li>Gilles San Martin vias Wikispecies</li>
+            <li>Malcolm Storey via BioImages</li>
+            <li>Peter O'Connor via Flickr CC </li>
+            <li>Trounce via Wikimedia</li>
+          </ul>
+        </P>
+
+        <P>
+          Icons made by{' '}
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik
+          </a>{' '}
+          from
+          <br />
+          <br />
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </P>
+        <P>
+          App guide illustrations made by:
+          <ul>
+            <li>
+              <a href="https://www.divulgare.net/" title="Divulgare">
+                www.divulgare.net
+              </a>
+            </li>
+
+            <li>
+              <a href="https://flic.kr/p/2k2HaKk" title="Flic">
+                https://flic.kr/p/2k2HaKk
+              </a>
+            </li>
+
+            <li>
+              <a href="hhttp://www.clker.com/" title="Clker">
+                http://www.clker.com/
+              </a>
+            </li>
+          </ul>
+        </P>
       </Section>
     </Main>
   </Page>
