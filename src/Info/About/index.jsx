89804074810701fb02fc9da0ee6e89af2c865943
Vilius Stankaitis <vilius@stankaitis.com>
2021-03-24 08:58:22 +0000

    #93 Update About page

--- src/Info/About/index.jsx
+++ src/Info/About/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Page, Main, Section, Header } from '@apps';
-import { IonList, IonImg, IonRow, IonGrid, IonCol } from '@ionic/react';
+import { IonIcon } from '@ionic/react';
+import { informationCircleOutline } from 'ionicons/icons';
 import appLogo from 'common/images/appLogo.png';
 import './styles.scss';
 
@@ -22,30 +23,55 @@ const About = () => (
         </P>
         <P>
           FIT Counts are suitable for all, in urban or rural locations, and can
-          be done at any time between the beginning of April and end of
-          September.
+          be done at any time during the FIT Count season – see the quick guide{' '}
+          <IonIcon icon={informationCircleOutline} /> for the season in your
+          country.
         </P>
         <P>
-          Wild pollinators may have declined in the UK by more than 30% since
-          1980, but we need much more data to be able to track changes in
-          abundance. You can help by doing a FIT Count, maybe even repeating it
-          over the season. You don’t need to identify the insects to species
+          An increasing amount of evidence suggests that pollinator species
+          diversity has declined in many parts of the world, with the strongest
+          evidence coming from Europe and North America. Less is known about
+          changes in pollinator abundance, and we need much more data to be able
+          to track this. You can help by doing a FIT Count, maybe even repeating
+          it over the season. You don’t need to identify the insects to species
           level, only to within broad groups e.g. bumblebees, hoverflies,
-          butterflies & moths, wasps
+          butterflies & moths, wasps.
         </P>
         <P>
-          The FIT Count is part of the UK Pollinator Monitoring Scheme (PoMS),
-          within the UK Pollinator Monitoring and Research Partnership which
-          comprises the UK Centre for Ecology & Hydrology (UKCEH), Bumblebee
-          Conservation Trust, Butterfly Conservation, British Trust for
-          Ornithology, Hymettus, Reading University, University of Leeds and
-          Natural History Museum. PoMS is jointly funded by Defra, the Welsh and
-          Scottish Governments, Daera, JNCC and project partners.
+          The FIT Count was originally developed through the UK Pollinator
+          Monitoring Scheme (PoMS). Similar methods are now being adopted in
+          other countries, and this app is used by a wide range of pollinator
+          monitoring projects:
         </P>
 
+        <ul>
+          <li>
+            UK Pollinator Monitoring Scheme{' '}
+            <a href="https://ukpoms.org.uk">ukpoms.org.uk</a>
+          </li>
+          <li>
+            Pollinator Monitoring Scheme Kýpros: PoMS-Ký
+            <a href="https://www.ris-ky.info/poms-ky">
+              https://www.ris-ky.info/poms-ky
+            </a>
+          </li>
+          <li>
+            Safeguarding Pollinators and Pollination Services in South America:
+            SURPASS2{' '}
+            <a href="https://bee-surpass.org">https://bee-surpass.org</a>
+          </li>
+          <li>
+            All-Ireland Pollinator Plan:{' '}
+            <a href="https://pollinators.ie">https://pollinators.ie</a>
+          </li>
+        </ul>
+
         <P>
-          For further information about PoMS go to:{' '}
-          <a href="https://ukpoms.org.uk">ukpoms.org.uk</a>
+          The FIT Count project is supported by the UK Centre for Ecology &
+          Hydrology and can be contacted here:{' '}
+          <a href="https://fitcount.ceh.ac.uk/contact">
+            https://fitcount.ceh.ac.uk/contact
+          </a>
         </P>
       </Section>
     </Main>
