cdd0b6a509af9a4c9b2ef035c2a84b3226e905fc
vilius-stankaitis <vilius@stankaitis.com>
2023-06-13 14:23:23 +0300

    Enable android back button

--- src/common/flumens.js
+++ src/common/flumens.js
@@ -52,3 +52,8 @@ export * from '@flumens/ionic/dist/utils/errors';
 export { default as date } from '@flumens/ionic/dist/utils/date';
 export { default as initAnalytics } from '@flumens/ionic/dist/utils/analytics';
 export { default as device } from '@flumens/ionic/dist/utils/device';
+export {
+  useDisableBackButton,
+  useOnBackButton,
+  useOnHideModal,
+} from '@flumens/ionic/dist/hooks/navigation';
