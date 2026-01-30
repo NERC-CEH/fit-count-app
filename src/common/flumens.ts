33f4b01a35ccb9460d06dcdcf753a97f1d63b75b
Karolis Kazlauskis <karolis@flumens.io>
2026-01-30 13:10:18 +0200

    Clean up

--- src/common/flumens.ts
+++ src/common/flumens.ts
@@ -1,64 +1,5 @@
-export { options as sentryOptions } from '@flumens/utils/dist/sentry';
-export { default as Main } from '@flumens/ionic/dist/components/Main';
-export { default as Page } from '@flumens/ionic/dist/components/Page';
-export { default as Header } from '@flumens/ionic/dist/components/Header';
-export { default as RouteWithModels } from '@flumens/ionic/dist/components/RouteWithModels';
-export { default as ModalHeader } from '@flumens/ionic/dist/components/ModalHeader';
-export {
-  default as Attr,
-  type Props as AttrProps,
-} from '@flumens/ionic/dist/components/Attr';
-export {
-  default as AttrPage,
-  type Props as PageProps,
-} from '@flumens/ionic/dist/components/AttrPage';
-export {
-  default as MenuAttrItemFromModel,
-  type MenuProps as MenuAttrItemFromModelMenuProps,
-} from '@flumens/ionic/dist/components/MenuAttrItemFromModel';
-export {
-  default as MapContainer,
-  useMapStyles,
-} from '@flumens/tailwind/dist/components/Map/Container';
+// MODELS
 export { type default as ElasticSample } from '@flumens/models/dist/Indicia/ElasticSample.d';
-export * from '@flumens/tailwind/dist/components/Map/utils';
-export { default as MapHeader } from '@flumens/ionic/dist/components/Map/Header';
-export { default as MapSettingsPanel } from '@flumens/ionic/dist/components/Map/SettingsPanel';
-export { default as MapDraw } from '@flumens/tailwind/dist/components/Map/Draw';
-export { default as CircleMarker } from '@flumens/tailwind/dist/components/Map/Container/LocationMarker/CircleMarker';
-export { default as Gallery } from '@flumens/ionic/dist/components/Gallery';
-export {
-  default as RadioInput,
-  type RadioOption,
-} from '@flumens/tailwind/dist/components/Radio';
-export { default as CheckboxInput } from '@flumens/tailwind/dist/components/Checkbox';
-export { default as NumberInput } from '@flumens/tailwind/dist/components/NumberInput';
-export { default as LongPressFabButton } from '@flumens/ionic/dist/components/LongPressFabButton';
-export { default as VirtualList } from '@flumens/tailwind/dist/components/VirtualList';
-export {
-  default as Input,
-  type Props as InputProps,
-} from '@flumens/tailwind/dist/components/Input';
-export { default as PhotoPicker } from '@flumens/ionic/dist/components/PhotoPicker';
-export * from '@flumens/utils/dist/date';
-export { default as device } from '@flumens/utils/dist/device';
-export * from '@flumens/utils/dist/uuid';
-export { useToast, useAlert, useLoader } from '@flumens/ionic/dist/hooks';
-export { default as Collapse } from '@flumens/ionic/dist/components/Collapse';
-export { default as InfoMessage } from '@flumens/tailwind/dist/components/InfoMessage';
-export { default as Badge } from '@flumens/tailwind/dist/components/Badge';
-export { default as Button } from '@flumens/tailwind/dist/components/Button';
-export { default as InfoBackgroundMessage } from '@flumens/tailwind/dist/components/InfoBackgroundMessage';
-export { default as InfoButton } from '@flumens/ionic/dist/components/InfoButton';
-export { default as Section } from '@flumens/ionic/dist/components/Section';
-export {
-  default as MenuAttrItem,
-  type Props as MenuAttrItemProps,
-} from '@flumens/ionic/dist/components/MenuAttrItem';
-export { default as Toggle } from '@flumens/tailwind/dist/components/Switch';
-export { default as Select } from '@flumens/tailwind/dist/components/Select';
-export { default as ImageCropper } from '@flumens/ionic/dist/components/ImageCropper';
-export { default as ModelValidationMessage } from '@flumens/ionic/dist/components/ModelValidationMessage';
 export { default as SampleCollection } from '@flumens/models/dist/Indicia/SampleCollection';
 export {
   default as GroupCollection,
@@ -101,18 +42,6 @@ export {
   default as DrupalUserModel,
   type Data as DrupalUserModelAttrs,
 } from '@flumens/models/dist/Drupal/User';
-// export { default as UserFeedbackRequest } from '@flumens/ionic/dist/components/UserFeedbackRequest';
-export {
-  useDisableBackButton,
-  useOnBackButton,
-  useOnHideModal,
-} from '@flumens/ionic/dist/hooks/navigation';
-export * from '@flumens/utils/dist/errors';
-export * from '@flumens/utils/dist/location';
-export * from '@flumens/utils/dist/image';
-export type * from '@flumens/utils/dist/type';
-export { default as ImageWithBackground } from '@flumens/ionic/dist/components/ImageWithBackground';
-export { default as UUID, hashCode } from '@flumens/utils/dist/uuid';
 export { default as Store } from '@flumens/models/dist/Stores/SQLiteStore';
 export { default as Collection } from '@flumens/models/dist/Collection';
 export {
@@ -125,6 +54,77 @@ export {
   type Media as ElasticOccurrenceMedia,
 } from '@flumens/models/dist/Indicia/ElasticOccurrence.d';
 
+// IONIC
+export { default as Main } from '@flumens/ionic/dist/components/Main';
+export { default as Page } from '@flumens/ionic/dist/components/Page';
+export { default as Header } from '@flumens/ionic/dist/components/Header';
+export { default as RouteWithModels } from '@flumens/ionic/dist/components/RouteWithModels';
+export { default as ModalHeader } from '@flumens/ionic/dist/components/ModalHeader';
+export {
+  default as Attr,
+  type Props as AttrProps,
+} from '@flumens/ionic/dist/components/Attr';
+export {
+  default as AttrPage,
+  type Props as PageProps,
+} from '@flumens/ionic/dist/components/AttrPage';
+export {
+  default as MenuAttrItemFromModel,
+  type MenuProps as MenuAttrItemFromModelMenuProps,
+} from '@flumens/ionic/dist/components/MenuAttrItemFromModel';
+export { default as MapHeader } from '@flumens/ionic/dist/components/Map/Header';
+export { default as MapSettingsPanel } from '@flumens/ionic/dist/components/Map/SettingsPanel';
+export { default as Gallery } from '@flumens/ionic/dist/components/Gallery';
+export { default as LongPressFabButton } from '@flumens/ionic/dist/components/LongPressFabButton';
+export { default as PhotoPicker } from '@flumens/ionic/dist/components/PhotoPicker';
+export { useToast, useAlert, useLoader } from '@flumens/ionic/dist/hooks';
+export { default as Collapse } from '@flumens/ionic/dist/components/Collapse';
+export { default as InfoButton } from '@flumens/ionic/dist/components/InfoButton';
+export { default as Section } from '@flumens/ionic/dist/components/Section';
+export {
+  default as MenuAttrItem,
+  type Props as MenuAttrItemProps,
+} from '@flumens/ionic/dist/components/MenuAttrItem';
+export { default as ImageCropper } from '@flumens/ionic/dist/components/ImageCropper';
+export { default as ModelValidationMessage } from '@flumens/ionic/dist/components/ModelValidationMessage';
+export {
+  useDisableBackButton,
+  useOnBackButton,
+  useOnHideModal,
+} from '@flumens/ionic/dist/hooks/navigation';
+export { default as ImageWithBackground } from '@flumens/ionic/dist/components/ImageWithBackground';
+export {
+  default as useSample,
+  withSample,
+  SamplesContext,
+} from '@flumens/ionic/dist/hooks/useSample';
+export { default as useRemoteSample } from '@flumens/ionic/dist/hooks/useRemoteSample';
+
+// TAILWIND
+export {
+  default as MapContainer,
+  useMapStyles,
+} from '@flumens/tailwind/dist/components/Map/Container';
+export * from '@flumens/tailwind/dist/components/Map/utils';
+export { default as MapDraw } from '@flumens/tailwind/dist/components/Map/Draw';
+export { default as CircleMarker } from '@flumens/tailwind/dist/components/Map/Container/LocationMarker/CircleMarker';
+export {
+  default as RadioInput,
+  type RadioOption,
+} from '@flumens/tailwind/dist/components/Radio';
+export { default as CheckboxInput } from '@flumens/tailwind/dist/components/Checkbox';
+export { default as NumberInput } from '@flumens/tailwind/dist/components/NumberInput';
+export { default as VirtualList } from '@flumens/tailwind/dist/components/VirtualList';
+export {
+  default as Input,
+  type Props as InputProps,
+} from '@flumens/tailwind/dist/components/Input';
+export { default as InfoMessage } from '@flumens/tailwind/dist/components/InfoMessage';
+export { default as Badge } from '@flumens/tailwind/dist/components/Badge';
+export { default as Button } from '@flumens/tailwind/dist/components/Button';
+export { default as InfoBackgroundMessage } from '@flumens/tailwind/dist/components/InfoBackgroundMessage';
+export { default as Toggle } from '@flumens/tailwind/dist/components/Switch';
+export { default as Select } from '@flumens/tailwind/dist/components/Select';
 export {
   default as TailwindContext,
   type ContextValue as TailwindContextValue,
@@ -138,9 +138,14 @@ export {
   default as TailwindBlockContext,
   defaultContext,
 } from '@flumens/tailwind/dist/components/Block/Context';
-export {
-  default as useSample,
-  withSample,
-  SamplesContext,
-} from '@flumens/ionic/dist/hooks/useSample';
-export { default as useRemoteSample } from '@flumens/ionic/dist/hooks/useRemoteSample';
+
+// UTILS
+export { options as sentryOptions } from '@flumens/utils/dist/sentry';
+export * from '@flumens/utils/dist/date';
+export { default as device } from '@flumens/utils/dist/device';
+export * from '@flumens/utils/dist/uuid';
+export * from '@flumens/utils/dist/errors';
+export * from '@flumens/utils/dist/location';
+export * from '@flumens/utils/dist/image';
+export type * from '@flumens/utils/dist/type';
+export { default as UUID, hashCode } from '@flumens/utils/dist/uuid';
